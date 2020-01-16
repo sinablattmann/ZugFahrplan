a3deabbd0d0ea7c38340b74247a25c7bddc509b6
Sina <sina.blattmann@nyp.ch>
2020-01-16 14:37:46 +0100

    added test class, no tests implemented yet

---
+++ src/ch/noseryoung/plj/test/DepartureTest.java
@@ -0,0 +1,12 @@
+package ch.noseryoung.plj.test;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DepartureTest {
+  @Test
+  public void getDeparturesTest() {
+
+  }
+}
